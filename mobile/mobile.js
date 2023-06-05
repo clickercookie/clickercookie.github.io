508f60f2b794d73efb78688c2d890da1b0986bd9
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2023-06-05 16:19:39 -0400

    Broken Options area

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -45,16 +45,22 @@ function navbarItemClicked(item) {
     document.getElementById("storeFooterItem").style.scale = "100%";
     document.getElementById("statsFooterItem").style.scale = "100%";
     document.getElementById("optionsFooterItem").style.scale = "100%";
+    // document.getElementById("cookieFooterText").style.color = "gray";
+    // document.getElementById("storeFooterText").style.color = "gray";
+    // document.getElementById("statsFooterText").style.color = "gray";
+    // document.getElementById("optionsFooterText").style.color = "gray";
     switch (item) {
         case "Cookie":
             document.getElementById("cookieContent").style.display = "flex";
             content.style.background = "url(../img/backgrounds/background-blue.png)";
             document.getElementById("cookieFooterItem").style.scale = "110%";
+            // document.getElementById("cookieFooterText").style.color = "white";
             break;
         case "Store":
             document.getElementById("storeContent").style.display = "flex";
             content.style.background = filtered;
             document.getElementById("storeFooterItem").style.scale = "110%";
+            // document.getElementById("storeFooterText").style.color = "white";
             break;
         case "Stats":
             document.getElementById("statsContent").style.display = "flex";
