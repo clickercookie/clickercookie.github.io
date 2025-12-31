9cf742a0590fabc11617cb107107b411b1366224
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2025-12-31 18:35:17 -0500

    style: Nest `main.scss`

--- src/ts/changelogs.ts
+++ src/ts/changelogs.ts
@@ -269,7 +269,7 @@ export const versionChangelogs: Changelog[] = [
 ];
 
 export function createChangelogEntry(logs: Changelog) { // todo 0.7: try to make this look nicer, but lets be honest it's gonna stay this way
-    const changelog = document.querySelector(".changelog-wrapper");
+    const changelog = document.getElementById("changelogWrapper");
 
     const newChangelogEntry = document.createElement("div");
     newChangelogEntry.setAttribute("class","changelog");
